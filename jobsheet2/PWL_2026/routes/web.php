584d44e723e2f008741233f67c851c4a71abef7a
athfizh <athaullahafizh@gmail.com>
2026-02-23 10:55:56 +0700

    praktikum 2
    Membuat Controller

--- jobsheet2/PWL_2026/routes/web.php
+++ jobsheet2/PWL_2026/routes/web.php
@@ -1,21 +1,20 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
 
-Route::get('/hello', function () {
-    return 'Hello World';
-});
+Route::get('/hello', [WelcomeController::class, 'hello']);
 
 Route::get('/world', function () {
     return 'World';
 });
 
-Route::get('/', function () {
-    return 'Selamat Datang';
-});
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/about', function () {
-    return 'NIM: 244107020030, Nama: Athaulla Hafizh';
-});
+Route::get('/about', [AboutController::class, 'about']);
 
 Route::get('/user/{name?}', function ($name = 'John') {
     return 'Nama saya ' . $name;
@@ -25,6 +24,18 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
 });
 
-Route::get('/articles/{id}', function ($id) {
-    return "Halaman Artikel dengan ID " . $id;
-});
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index',
+    'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create',
+    'store',
+    'update',
+    'destroy'
+]);
